8e09cf0c2025fca2ba03f650d7a22c91460bd73e
Bernd von Platen | ene't <BerndV@enet.eu>
2020-05-31 11:02:21 +0200

    GeoPy.py added

---
+++ GeoPy.py
@@ -0,0 +1,38 @@
+# GeoPy.py 
+# playing with geometric math (triangle)
+#
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# 31.05.2019  bvp  initial release
+#
+import turtle as t
+
+#
+#
+#
+def dreieck( laenge = 100.0, winkel = 120.0 ):
+    for i in range(3):
+        t.forward(laenge)
+        t.left(winkel)
+
+
+#
+#
+#
+def quadrat( laenge = 100.0, winkel = 90.0 ):
+    for i in range(4):
+        t.forward(laenge)
+        t.left(winkel)
+
+
+def main():
+    laenge = 100
+    for i in range(4):
+        dreieck(laenge)
+        quadrat(laenge)
+        laenge += 10 
+    
+if __name__ == "__main__":
+    main()
+
+
+
